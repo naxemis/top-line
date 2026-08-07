aa66e22e3365e2d27180e5290636baa77c4f47f5
naxemis <szymon.lenard@icloud.com>
2026-08-07 19:01:01 +0200

    refactor(ui): make run_current_page a private method

--- ui/app.py
+++ ui/app.py
@@ -10,14 +10,15 @@ class App:
     def __init__(self, page: flet.Page):
         self.page = page
         self.current_page: Pages = Pages.START
+        self._run_current_page()
 
     def change_page(self, new_page: Pages):
         self.current_page = new_page
-        self.run_current_page()
+        self._run_current_page()
 
-    def run_current_page(self):
-        page_class = self.current_page.value
-        page_instance = page_class(self.page)
+    def _run_current_page(self):
+        page_class = self.current_page.value # retrieve the page class stored as the enum value (line 6)
+        page_instance = page_class(self.page) # then instantiate it by passing the main flet page instance
 
         self.page.clean()
         page_instance.run()
--- ui/main.py
+++ ui/main.py
@@ -3,8 +3,7 @@ import flet
 from ui.app import App
 
 def main(page: flet.Page):
-    app = App(page)
-    app.run_current_page()
+    App(page)
 
 if __name__ == "__main__":
     flet.app(target=main)
