72920f0f2562bfb2930b3b1c0962ab957129ddfd
naxemis <szymon.lenard@icloud.com>
2026-08-07 19:16:28 +0200

    feat(ui): initialize page setup during app init

--- ui/app.py
+++ ui/app.py
@@ -9,8 +9,24 @@ class Pages(Enum):
 class App:
     def __init__(self, page: flet.Page):
         self.page = page
+
+        self._setup_page()
+
         self.current_page: Pages = Pages.START
         self._run_current_page()
+
+    def _setup_page(self):
+        self.page.title = "TopLine"
+        self.page.theme_mode = flet.ThemeMode.SYSTEM
+
+        self.page.window.height = 480
+        self.page.window.width = 640
+        self.page.window.resizable = False
+
+        self.page.vertical_alignment = flet.MainAxisAlignment.CENTER
+        self.page.horizontal_alignment = flet.CrossAxisAlignment.CENTER
+
+        self.page.update()
 
     def change_page(self, new_page: Pages):
         self.current_page = new_page
--- ui/pages/start.py
+++ ui/pages/start.py
@@ -4,7 +4,6 @@ from ui.pages.abstract.page import Page
 
 class StartPage(Page):
     def run(self):
-        self.page.title = "Start Page"
         self.page.add(
             flet.Text("Welcome to the Start Page!")
         )
