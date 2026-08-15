8195316c78c4d491cc193def48f4d4c92c1f6dfa
naxemis <szymon.lenard@icloud.com>
2026-08-15 15:29:20 +0200

    feat(models): introduce license model

---
+++ TopLine.Core/Models/License.cs
@@ -0,0 +1,11 @@
+namespace TopLine.Core.Models;
+
+public class License
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = String.Empty;
+    public string Client { get; set; } = String.Empty;
+    public string? ContactInfo { get; set; }
+    public bool AllRightsReserved { get; set; } = false;
+    public int Year { get; set; } = DateTime.Now.Year;
+}
