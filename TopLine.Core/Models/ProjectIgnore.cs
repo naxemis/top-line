595a62aa44c2adf131c0b43638e8101c101ea430
naxemis <szymon.lenard@icloud.com>
2026-08-15 15:45:08 +0200

    feat(models): introduce project ignore and project license models

---
+++ TopLine.Core/Models/ProjectIgnore.cs
@@ -0,0 +1,10 @@
+namespace TopLine.Core.Models;
+
+// ProjectId, TargetType, and Target are unique
+public class ProjectIgnore
+{
+    public int Id { get; set; }
+    public int ProjectId { get; set; }
+    public TargetType TargetType { get; set; }
+    public string Target { get; set; } = String.Empty;
+}
---
+++ TopLine.Core/Models/ProjectLicense.cs
@@ -0,0 +1,11 @@
+namespace TopLine.Core.Models;
+
+// ProjectId, TargetType, and Target are unique
+public class ProjectLicense
+{
+    public int Id { get; set; }
+    public int ProjectId { get; set; }
+    public int LicenseId { get; set; }
+    public TargetType TargetType { get; set; }
+    public string Target { get; set; } = String.Empty;
+}
